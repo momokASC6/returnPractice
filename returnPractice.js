1f1bae44e5e339da05e5fba8468aea7435138eff
momokASC6 <mikuniku3@gmail.com>
2019-07-12 09:46:38 -0400

    Creates function with return value

---
+++ returnPractice.js
@@ -0,0 +1,13 @@
+//by default all JS functions return a value
+//if the return value is not specified the function returns undefined
+
+function someFunc() {
+    //return syntax
+    return "Hello World";
+    console.log("Will this code print");
+    //return 5;
+}
+
+const result = someFunc();
+
+console.log(result);
